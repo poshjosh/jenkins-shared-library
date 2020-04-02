86770c77c4f073da95a385dbd5e1443da435bbe8
poshjosh <posh.bc@gmail.com>
2020-04-02 04:39:03 +0100

    Print container logs after running the app container

--- vars/completePipeline.groovy
+++ vars/completePipeline.groovy
@@ -266,6 +266,7 @@ def call(Map config=[:]) {
                                         // SERVER_URL is an environment variable not a pipeline parameter
                                         if(env.SERVER_URL) {
                                             sleep 10
+                                            sh "docker container logs ${CONTAINER_NAME}"
                                             sh "curl --retry 3 --retry-connrefused --connect-timeout 30 --max-time 60 ${SERVER_URL}"
                                         }else {
                                             echo "No Server URL"
